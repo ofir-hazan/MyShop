51f9f0daf321c857c1c0597f72af1d6b6ffa2004
gmesh <gmeshie@gmail.com>
2022-11-21 20:58:57 +0200

    Add products data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import cake from './assets/cake.jpg'
+import candy from './assets/candy.jpg'
+import chocolate from './assets/chocolate.jpg'
+import lollipops from './assets/lollipops.webp'
+import marshmallows from './assets/marshmallows.jpg'
+
 function App() {
+
+  const products = [
+    { 
+      title: 'marshmallows', price: 8, image: marshmallows
+    },
+    { 
+      title: 'chocolate', price: 10, image: chocolate,
+    },
+    { 
+      title: 'cake', price: 20, image: cake,
+    },
+    { 
+      title: 'candy', price: 15, image: candy
+    },
+    { 
+      title: 'lollipop', price: 5, image: lollipops,
+    },
+  ];
 
   return (
     <div>
